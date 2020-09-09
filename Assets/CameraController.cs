a2616cb7c4d021a940a902755cadf2dc8e27f8ca
Ezharjan <ezharjan@aliyun.com>
2020-09-09 09:29:57 +0800

    done

---
+++ Assets/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using System.IO;
+
+public class CameraController : MonoBehaviour
+{
+    /// <summary>
+    /// 外部摄像头
+    /// </summary>
+    private WebCamTexture webTex;
+    /// <summary>
+    /// UI父物体
+    /// </summary>
+    private Canvas canvas;
+    /// <summary>
+    /// 摄像头映射画面
+    /// </summary>
+    private RawImage Camera_image;
+
+
+
+
+    private void Start()
+    {
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        Camera_image = canvas.transform.Find("Camera_screen").GetComponent<RawImage>();
+
+        StartCoroutine(CallCamera());
+    }
+
+
+    /// <summary>
+    /// 打开摄像头
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator CallCamera()
+    {
+        //等待用户允许访问
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        //如果用户允许访问，开始获取图像        
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+            string devicename = devices[0].name;
+            webTex = new WebCamTexture(devicename, Screen.width, Screen.height);
+            Camera_image.texture = webTex;
+            webTex.Play();
+        }
+    }
+}
---
+++ Assets/ShaderController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class ShaderController : MonoBehaviour
+{
+
+    float sensS, cutoffS;
+    Color colS;
+
+
+
+    void Start()
+    {
+        sensS = GetComponent<RawImage>().material.GetFloat("_Sens");
+        cutoffS = GetComponent<RawImage>().material.GetFloat("_Cutoff");
+        colS = GetComponent<RawImage>().material.GetColor("_Color");
+
+        sens = sensS;
+        cutoff = cutoffS;
+    }
+
+
+    public float sens, cutoff;
+    public string r = "99", g = "205", b = "77";
+    void OnGUI()
+    {
+        sens = GUI.HorizontalSlider(new Rect(25, 25, 100, 30), sens, 0.0f, 1.0f);
+        cutoff = GUI.HorizontalSlider(new Rect(25, 70, 100, 30), cutoff, 0.0f, 1.0f);
+
+        r = GUI.TextField(new Rect(25, 120, 40, 20), r);
+        g = GUI.TextField(new Rect(70, 120, 40, 20), g);
+        b = GUI.TextField(new Rect(120, 120, 40, 20), b);
+
+        if (GUI.Button(new Rect(25, 160, 100, 30), "Reset"))
+        {
+            sens = sensS;
+            cutoff = cutoffS;
+
+            r = (colS.r * 255f).ToString();
+            g = (colS.g * 255f).ToString();
+            b = (colS.b * 255f).ToString();
+        }
+
+
+        GetComponent<RawImage>().material.SetFloat("_Sens", sens);
+        GetComponent<RawImage>().material.SetFloat("_Cutoff", cutoff);
+        try
+        {
+            Color col = new Color(int.Parse(r) / 255f, int.Parse(g) / 255f, int.Parse(b) / 255f);
+            //print (col);
+            GetComponent<RawImage>().material.color = col;
+
+        }
+        catch (UnityException e)
+        {
+        }
+    }
+}
